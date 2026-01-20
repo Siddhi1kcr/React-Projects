dbe9437a2cd2d17f692dc36a56bed2b8d433f30d
Siddhi <siddhi1kcr@gmail.com>
2026-01-20 23:25:28 +0530

    added env file and setting up the config folder

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   return (
     <>
       <h1>A blog app with Appwrite</h1>
