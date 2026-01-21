3b4624aa3f7093735870964126d9039ba92328ae
Siddhi <siddhi1kcr@gmail.com>
2026-01-21 22:39:42 +0530

    Built Authentication Service with appwrite

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    
+    async createAccount({email, password, name}){
+        try {
+            const user = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+            if(user){
+                return user;
+            }
+        } catch (error) {
+            console.log("Appwrite service :: createAccount :: error", error);
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(
+                email,
+                password
+            );
+        }
+        catch (error) {
+            console.log("Appwrite service :: login :: error", error);   
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        }   
+        catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);   
+        }
+    }
+
+    async logout(){
+        try{
+            return await this.account.deleteSessions();
+        }
+        catch (error) {
+            console.log("Appwrite service :: logout :: error", error);   
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
+
